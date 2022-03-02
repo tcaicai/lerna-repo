df7d76be2cead3bbaa9e3f2dd96c01d9ad1bcd2d
caicai <guangliyan@zuoyebang.com>
2022-03-02 15:37:26 +0800

    feat: bind 构造函数

--- test_bind.js
+++ test_bind.js
@@ -1,10 +1,18 @@
 function bind(obj, ...arg) {
   obj = typeof obj === "object" && obj !== null ? obj : window;
   let _this = this;
-  return function (...args) {
+
+  function F() {}
+  const Fn = function (...args) {
     let allArs = [...arg, ...args];
+    if (new.target) {
+      obj = this;
+    }
     return _this.call(obj, ...allArs);
   };
-}
+  F.prototype = this.prototype;
+  Fn.prototype = new F();
+  return Fn;
+};
 
 module.exports = bind;
